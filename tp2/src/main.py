edd98c0cbf299713f267959167dc09059803a466
Dafydd Jenkins <dafydd.a.jenkins@gmail.com>
2024-11-11 16:41:39 -0300

    comentarios y docstrings TP2

--- tp2/src/main.py
+++ tp2/src/main.py
@@ -1,13 +1,17 @@
 from railway_service import *
 import os
 
-#crear y mostrar el grafo como se propone en el enucniado
+############################################################
+# CREAR Y MOSTRAR EL GRAFO COMO SE PROPONE EN EL ENUNCIADO #
+############################################################
 file_name = os.path.join(os.path.dirname(__file__), '../instances/toy_instance.json')
 data = load_instance(file_name)
 grafo, nodos_estacion = create_graph(data)
 #show_graph(grafo, data, nodos_estacion)
 
-#resolver el problema y mostrar la respuesta como flujo
+######################################################
+# RESOLVER EL PROBLEMA Y MOSTRAR EL FLUJO RESULTANTE #
+######################################################
 cost, circulacion = solve_circulacion(grafo)
 print("Costo de la circulación: ", cost)
-show_flow(grafo, circulacion, data, nodos_estacion)
+#show_flow(grafo, circulacion, data, nodos_estacion)
--- tp2/src/railway_service.py
+++ tp2/src/railway_service.py
@@ -4,75 +4,85 @@ from math import ceil
 import matplotlib.pyplot as plt
 
 def load_instance(filename):
+    '''Cargar una instancia de un archivo JSON'''
     with open(filename) as json_file:
         data = json.load(json_file)
     return data
 
 def create_graph(data):
+    '''Crea el grafo del problema de circulación a partir de la información de la instancia.
+        Devuelve el grafo y un diccionario con los tiempos de salida y llegada de cada estación ordenados de menor a mayor.
+    '''
     G = nx.DiGraph()
-    services = data["services"]
-    rs_capacity = data["rs_info"]["capacity"]
-    max_rs = data["rs_info"]["max_rs"]
-    a = data["stations"][0]
-    b = data["stations"][1]
+    services = data["services"] # Diccionario con la información de los servicios
+    rs_capacity = data["rs_info"]["capacity"] # Capacidad de los RS
+    max_rs = data["rs_info"]["max_rs"] # Máximo de RS que se pueden usar
+    a = data["stations"][0] # Estación 'a'
+    b = data["stations"][1] # Estación 'b'
 
-    nodos_estacion = {a: [], b: []}
+    nodos_estacion = {a: [], b: []} # Diccionario con los tiempos de salida y llegada de cada estación
 
     for service in services:
-        stops = services[service]["stops"]
+        stops = services[service]["stops"] 
         salida = stops[0]
-        salida_str = salida["station"]+"_"+str(salida["time"])
+        salida_str = salida["station"]+"_"+str(salida["time"]) # Identificador del nodo de salida
         llegada = stops[1]
-        llegada_id = llegada["station"]+"_"+str(llegada["time"])
-        needed_rs = ceil(services[service]["demand"][0]/rs_capacity)
-        nodos_estacion[salida["station"]].append(salida["time"])
-        nodos_estacion[llegada["station"]].append(llegada["time"])
+        llegada_id = llegada["station"]+"_"+str(llegada["time"]) # Identificador del nodo de llegada
+        needed_rs = ceil(services[service]["demand"][0]/rs_capacity) # RS necesarios para el servicio
+        nodos_estacion[salida["station"]].append(salida["time"]) # Agregar el tiempo de salida a la lista de tiempos de la estación
+        nodos_estacion[llegada["station"]].append(llegada["time"]) # Agregar el tiempo de llegada a la lista de tiempos de la estación
 
-        G.add_node(salida_str, demand = 0)
-        G.add_node(llegada_id, demand = 0)
-        G.add_edge(salida_str, llegada_id, weight = 0, lower_bound = needed_rs, upper_bound = max_rs)
+        G.add_node(salida_str, demand = 0) # Crear nodo de salida
+        G.add_node(llegada_id, demand = 0) # Crear nodo de llegada
+        G.add_edge(salida_str, llegada_id, weight = 0, lower_bound = needed_rs, upper_bound = max_rs) # Crear arista entre la salida y la llegada
 
     nodos_estacion[a].sort()
     nodos_estacion[b].sort()
 
+    # Crear aristas entre los nodos de la misma estación
     for i in range(len(nodos_estacion[a])-1):
         G.add_edge(a+"_"+str(nodos_estacion[a][i]), a+"_"+str(nodos_estacion[a][i+1]), weight = 0, lower_bound = 0, upper_bound = 1e9)
         G.add_edge(b+"_"+str(nodos_estacion[b][i]), b+"_"+str(nodos_estacion[b][i+1]), weight = 0, lower_bound = 0, upper_bound = 1e9)
     
+    # Crear aristas entre el último y el primer nodo de cada estación
     G.add_edge(a+"_"+str(nodos_estacion[a][-1]), a+"_"+str(nodos_estacion[a][0]), weight = 1, lower_bound = 0, upper_bound = 1e9)
     G.add_edge(b+"_"+str(nodos_estacion[b][-1]), b+"_"+str(nodos_estacion[b][0]), weight = 1, lower_bound = 0, upper_bound = 1e9)
 
     return G, nodos_estacion
 
 def transform_graph(G):
+    '''Transforma el grafo de circulación en un grafo de flujo de costo mínimo'''
     H = nx.DiGraph()
     for u, v, data in G.edges(data=True):
         l_bound = data["lower_bound"]
         u_bound = data["upper_bound"]
         weight = data["weight"]
+        # Añadir los mismos nodos de G a H
         H.add_node(u, demand = 0)
         H.add_node(v, demand = 0)
-        H.add_edge(u, v, weight = weight, capacity = u_bound - l_bound)
+        # Añadir aristas con nueva capacidad
+        H.add_edge(u, v, weight = weight, capacity = u_bound - l_bound) # 
 
+    #Calcular nuevos imbalances para cada nodo
     for u, v, data in G.edges(data=True):
         l_bound = data["lower_bound"]
         H.nodes[u]["demand"] += l_bound
         H.nodes[v]["demand"] -= l_bound
 
-    for node in H.nodes():
-        print(node, H.nodes[node]["demand"])
     return H
 
 def solve_circulacion(G):
+    '''Resolver el problema de circulación reduciéndolo a un problema de flujo de costo mínimo'''
     H = transform_graph(G)
-    # Resolver el problema de circulación con network_simplex
-    cost, circulacion = nx.network_simplex(H)
+    cost, circulacion = nx.network_simplex(H) # Resolver el problema de circulación con network_simplex
     for u in circulacion:
         for v in circulacion[u]:
-            circulacion[u][v] = circulacion[u][v] + G[u][v]["lower_bound"]
+            circulacion[u][v] = circulacion[u][v] + G[u][v]["lower_bound"] # Convertir la respuesta del problema de flujo de costo mínimo a la del problema de circulación
     return cost, circulacion
 
 def show_graph(G, data, nodos_estacion):
+    '''Mostrar un grafo G (de circulación o de flujo de costo mínimo) en dos columnas, con los nodos de la estación 'a' a la izquierda y los de la estación 'b' a la derecha'''
+
     # Crear un layout manual para organizar los nodos en dos columnas
     pos = {}
 
@@ -87,20 +97,22 @@ def show_graph(G, data, nodos_estacion):
         pos[node_id] = (2, -i)  # '2' para separar las columnas
 
     # Dibujar los nodos y las aristas
-    #nx.draw(G, pos, with_labels=True, node_size=700, node_color="lightblue", font_size=10, font_weight="bold", arrows=True)
     nx.draw_networkx_nodes(G, pos, node_size=400, node_color="lightblue")
     nx.draw_networkx_labels(G, pos, font_size=7, font_weight="regular")
     nx.draw_networkx_edges(G, pos,connectionstyle='arc3,rad=0.2', arrowstyle='-|>')
-    edge_labels = {(u, v): f"{data['lower_bound']}-{data['upper_bound']}" for u, v, data in G.edges(data=True)}
+    edge_labels = {}
+    for u, v, data in G.edges(data=True):
+        if ("lower_bound" in data) and ("upper_bound" in data): #Si es de circulación
+            edge_labels[(u, v)] = f"l: {data['lower_bound']} - c: {data['upper_bound']}"
+        elif ("capacity" in data): #Si es de flujo de costo mínimo
+            edge_labels[(u, v)] = f"u: {data['capacity']}"
     nx.draw_networkx_edge_labels(G, pos, connectionstyle='arc3,rad=0.2', edge_labels=edge_labels, font_size=7, font_color="red")
-    
-
-    # Crear y dibujar las etiquetas de las aristas con cotas
 
     # Mostrar el gráfico
     plt.show()
 
 def show_flow(G, flow, data, nodos_estacion):
+    '''Mostrar el flujo flow en el grafo G organizado en dos columnas, con los nodos de la estación 'a' a la izquierda y los de la estación 'b' a la derecha'''
     # Crear un layout manual para organizar los nodos en dos columnas
     pos = {}
 
